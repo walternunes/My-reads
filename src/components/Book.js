6e52c9fe52f60efeb68e2bdf6e0a109c2476bffd
Walter <jnunes.walter@gmail.com>
2017-12-17 14:46:19 -0200

    add update function to change book shelf

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
+import {update} from '../BooksAPI'
 
 class Book extends Component {
+
+  
+  updateBookShelf(book, event ) {
+    update(book, event).then(r =>{
+      console.log(r);
+    })
+  }
 
 
   render() {
     const { book } = this.props
-
     return (
       <li>
         <div className="book">
           <div className="book-top">
             <div className="book-cover" style={{ backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
             <div className="book-shelf-changer">
-              <select>
+              <select onChange={(event) => this.updateBookShelf(book, event.target.value)} defaultValue={book.shelf}>
                 <option value="none" disabled>Move to...</option>
                 <option value="currentlyReading">Currently Reading</option>
                 <option value="wantToRead">Want to Read</option>
